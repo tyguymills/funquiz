c1d8d2cc7a85a6a33ff3d5f613982a11c664792c
Tyler Miller <tyguymills@gmail.com>
2023-04-06 19:29:03 -0500

    initial commit

---
+++ style.js
@@ -0,0 +1,42 @@
+//  all div containers are classes
+// 
+// What do I need
+// var 
+// questions
+// game moves to next question after btn push
+// btn refreshed each time I click one
+// timer that cuts off when game is won or when time runs out
+// start game button
+//end game screen
+
+
+var startGame;
+var firstChoice = document;
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+var question1 = "question1";
+
+
+// start game btn
+
+
+
+// 
+var questions = [
+    {
+        question: "",
+        answers: "",
+        correct: ""
+    },
+];
+
+var
+
+
+// $("#btn1").textContent
+
+// determines if button is clicked
+
+
